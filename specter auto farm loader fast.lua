fb7f0acb62b251945a7052135dcd52e0b17f27d2
wizanthi <wizanthi1@gmail.com>
2025-10-22 08:39:15 +0300

    Update specter auto farm loader fast.lua

--- specter auto farm loader fast.lua
+++ specter auto farm loader fast.lua
@@ -1,1 +1,11 @@
+local scripts = 'https://raw.githubusercontent.com/wizanthi/WizanthiHub/main/scripts/'
 
+local market = game:GetService("MarketplaceService")
+local info = market:GetProductInfo(game.PlaceId)
+local place_id = game.PlaceId
+
+if place_id == 8267733039 then
+    loadstring(game:HttpGet(scripts.."specter%20lobby%20auto%20farm.lua"))()
+elseif info.Name == "SPECTER Classic [AI]" then
+    loadstring(game:HttpGet(scripts.."specter%20in%20game%20auto%20farm%20fast.lua"))()
+end
