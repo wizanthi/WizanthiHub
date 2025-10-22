5bfc263ad20ee006d750c6a582adf4ae840debfa
wizanthi <wizanthi1@gmail.com>
2025-10-22 17:49:36 +0300

    Fix script loading case sensitivity

--- specter auto farm loader slow.lua
+++ specter auto farm loader slow.lua
@@ -5,7 +5,7 @@ local info = market:GetProductInfo(game.PlaceId)
 local place_id = game.PlaceId
 
 if place_id == 8267733039 then
-    loadstring(game:HttpGet(scripts.."specter%20lobby%20auto%20farm.lua"))()
+    loadstring(game:HttpGet(scripts.."Specter%20Lobby%20Auto%20Farm.lua"))()
 elseif info.Name == "SPECTER Classic [AI]" then
-    loadstring(game:HttpGet(scripts.."specter%20in%20game%20auto%20farm%20slow.lua"))()
+    loadstring(game:HttpGet(scripts.."Specter%20Auto%20Farm%20Slow.lua"))()
 end
